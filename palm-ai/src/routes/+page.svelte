33b76a9145d69161783e3159db75d375ab376b09
ppaauuoo <bcg.channel@gmail.com>
2023-05-22 16:04:42 +0700

    palm ai (need money so unfin)

---
+++ palm-ai/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+<script>
+    import { initializeApp } from "firebase/app";
+    import { getFirestore } from "firebase/firestore"
+    
+    const firebaseConfig = {
+    apiKey: "AIzaSyC6EcognH07uj9BHzPOM-cSmooXZZCB99I",
+    authDomain: "svelte-56809.firebaseapp.com",
+    projectId: "svelte-56809",
+    storageBucket: "svelte-56809.appspot.com",
+    messagingSenderId: "425431799933",
+    appId: "1:425431799933:web:b3a837eee37429303d7914"
+  };
+    
+    const app = initializeApp(firebaseConfig);
+    
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
